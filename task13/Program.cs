e64683d8d9902c34718bcc4f36e563fba356acb8
Alsu Lidzhieva <magadieva.alsu@yandex.ru>
2022-12-25 22:51:52 +0500

    Добавили решение Task13

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number>100)
+    {int number3 = number%10;
+    number = number/10;
+    if (number <100)
+    {
+Console.WriteLine(number3);
+    }
+}
+}
+
